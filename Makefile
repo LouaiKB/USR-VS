94f2ebd6b4790ad0dc07eb402ad16bcf12f4d553
Jacky Lee <JackyLeeHongJian@Gmail.com>
2016-03-21 12:00:37 +0800

    Added embed

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 CC=clang++
 
-all: bin/encode bin/usr
+all: bin/embed bin/encode bin/usr
+
+bin/embed: obj/embed.o
+	${CC} -o $@ $^ -L${RDKIT_ROOT}/lib -lGraphMol -lFileParsers -lSmilesParse -lDistGeomHelpers
 
 bin/encode: obj/encode.o
 	${CC} -o $@ $^ -L${RDKIT_ROOT}/lib -lGraphMol -lFileParsers -lSmilesParse -lSubstructMatch -lRDGeneral
@@ -8,8 +11,11 @@ bin/encode: obj/encode.o
 bin/usr: obj/main.o obj/io_service_pool.o obj/safe_counter.o
 	${CC} -o $@ $^ -pthread -L${BOOST_ROOT}/lib -lboost_system -lboost_filesystem -lboost_date_time -L${RDKIT_ROOT}/lib -lRDGeneral -lGraphMol -lFileParsers -lSmilesParse -lFingerprints -lSubstructMatch -lAlignment -lMolTransforms -L${MONGODBCXXDRIVER_ROOT}/sharedclient -lmongoclient
 
+obj/embed.o: src/embed.cpp
+	${CC} -o $@ $< -c -std=c++14 -O2 -Wall -I${RDKIT_ROOT}/include/rdkit
+
 obj/encode.o: src/encode.cpp
-	${CC} -o $@ $< -c -std=c++14 -O2 -Wall -I${BOOST_ROOT} -I${RDKIT_ROOT}/include/rdkit
+	${CC} -o $@ $< -c -std=c++14 -O2 -Wall -I${RDKIT_ROOT}/include/rdkit
 
 obj/main.o: src/main.cpp
 	${CC} -o $@ $< -c -std=c++14 -O2 -Wall -Wno-unused-local-typedef -Wno-deprecated-declarations -Wno-deprecated-register -I${BOOST_ROOT} -I${RDKIT_ROOT}/include/rdkit -I${MONGODBCXXDRIVER_ROOT}/src
---
+++ src/embed.cpp
@@ -0,0 +1,37 @@
+#include <GraphMol/FileParsers/MolSupplier.h>
+#include <GraphMol/FileParsers/MolWriters.h>
+#include <GraphMol/SmilesParse/SmilesParse.h>
+#include <GraphMol/FragCatalog/FragFPGenerator.h>
+#include <GraphMol/DistGeomHelpers/Embedder.h>
+using namespace std;
+using namespace RDKit;
+using namespace RDKit::MolOps;
+using namespace RDKit::DGeomHelpers;
+
+int main(int argc, char* argv[])
+{
+	SmilesMolSupplier sup("query.smi", "\t", 0, -1, false);
+	if (!sup.length() || !sup.atEnd()) return 1;
+
+	// Obtain a pointer to the current molecule with heavy atoms only.
+	const unique_ptr<ROMol> smi_ptr(sup.next()); // Calling next() may print "ERROR: Could not sanitize molecule on line XXXX" to stderr.
+
+	// Add hydrogens to the molecule. To get good conformations, it's almost always a good idea to add hydrogens to the molecule first. http://rdkit.org/docs/GettingStartedInPython.html#writing-molecules
+	const unique_ptr<ROMol> mol_ptr(addHs(*smi_ptr));
+
+	// Obtain a reference to the molecule to avoid writing *mol_ptr.
+	auto& mol = *mol_ptr;
+
+	// Generate conformers with knowledge.
+	const auto confId = EmbedMolecule(mol, 0, -1, true, false, 2.0, true, 1, 0, 1e-3, false, true, true, true, false, 5.0);
+
+	// Check if conformers are generated.
+	if (confId == -1)
+	{
+		return 1;
+	}
+
+	// Create output streams.
+	SDWriter writer("query.sdf");
+	writer.write(mol);
+}
