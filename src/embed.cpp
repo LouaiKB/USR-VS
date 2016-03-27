0203d28c507d0a6b0541bf1375520fba1416f2db
Jacky Lee <JackyLeeHongJian@Gmail.com>
2016-03-27 14:51:45 +0800

    Added a comment

--- src/embed.cpp
+++ src/embed.cpp
@@ -17,6 +17,8 @@ int main(int argc, char* argv[])
 	// Pipe the line to the supplier.
 	istringstream iss(str);
 	SmilesMolSupplier sup(&iss, false, "\t", 0, -1, false);
+
+	// Validate the input.
 	if (!sup.length() || !sup.atEnd()) return 1;
 
 	// Obtain a pointer to the current molecule with heavy atoms only.
