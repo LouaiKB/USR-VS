f340f096b23fc685789b926df38d4a46883034a7
Jacky Lee <JackyLeeHongJian@Gmail.com>
2016-03-27 14:40:01 +0800

    Replaced SmilesMolSupplier sup(&cin,...); by SmilesMolSupplier sup(&iss,...);

--- src/embed.cpp
+++ src/embed.cpp
@@ -10,7 +10,13 @@ using namespace RDKit::DGeomHelpers;
 
 int main(int argc, char* argv[])
 {
-	SmilesMolSupplier sup(&cin, false, "\t", 0, -1, false);
+	// Read a line from standard input.
+	string str;
+	getline(cin, str);
+
+	// Pipe the line to the supplier.
+	istringstream iss(str);
+	SmilesMolSupplier sup(&iss, false, "\t", 0, -1, false);
 	if (!sup.length() || !sup.atEnd()) return 1;
 
 	// Obtain a pointer to the current molecule with heavy atoms only.
@@ -26,10 +32,7 @@ int main(int argc, char* argv[])
 	const auto confId = EmbedMolecule(mol, 0, -1, true, false, 2.0, true, 1, 0, 1e-3, false, true, true, true, false, 5.0);
 
 	// Check if conformers are generated.
-	if (confId == -1)
-	{
-		return 1;
-	}
+	if (confId == -1) return 1;
 
 	// Create output streams.
 	SDWriter writer(&cout);
